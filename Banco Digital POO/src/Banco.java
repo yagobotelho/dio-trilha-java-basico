7b90b09f8acd36464c9bf3db189613b26f1dd9dc
yagobotelho <86793874+yagobotelho@users.noreply.github.com>
2023-01-14 14:48:19 +0000

    Projeto Banco Digital POO

---
+++ Banco Digital POO/src/Banco.java
@@ -0,0 +1,9 @@
+public class Banco {
+    private String nome;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
