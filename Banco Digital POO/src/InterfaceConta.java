8cc5dcc06d81d3912d297fc887f863cf3877155b
yagobotelho <86793874+yagobotelho@users.noreply.github.com>
2023-01-14 14:45:43 +0000

    Add files via upload

---
+++ Banco Digital POO/src/InterfaceConta.java
@@ -0,0 +1,10 @@
+public interface InterfaceConta {
+
+    void sacar(double valor);
+
+    void depositar(double valor);
+
+    void transferir(double valor, InterfaceConta contaDestino);
+
+    void imprimirExtrato();
+}
